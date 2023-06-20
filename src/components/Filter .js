cc234d42e58b5775885b948b7b02e8cc5097684b
wael kamira <ramondm4@gmail.com>
2023-06-20 22:30:36 +0300

    Assignment Completed

---
+++ src/components/Filter .js
@@ -0,0 +1,13 @@
+function Filter({onCategoryChange}) {
+  return (
+    <div>
+      <select name="filter" onChange={onCategoryChange}>
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+    </div>
+  );
+}
+export default Filter;
